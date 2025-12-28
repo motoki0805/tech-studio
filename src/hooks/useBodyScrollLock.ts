28dfe87b06fbac143ed92a3e976dd3f623296cc8
nanashima.m <nanashima.m@tech-studio.jp>
2025-12-28 20:12:38 +0900

    fix: モーダルの表示の幅が適切になるように修正

--- src/hooks/useBodyScrollLock.ts
+++ src/hooks/useBodyScrollLock.ts
@@ -4,20 +4,21 @@ import { useEffect } from "react";
 
 export const useBodyScrollLock = (isLocked: boolean) => {
   useEffect(() => {
-    if (typeof window === "undefined") return;
-
-    const originalOverflow = document.body.style.overflow;
-    const originalPaddingRight = document.body.style.paddingRight;
-
-    if (isLocked) {
-      const scrollbarWidth =
-        window.innerWidth - document.documentElement.clientWidth;
-      document.body.style.overflow = "hidden";
-      document.body.style.paddingRight = `${scrollbarWidth}px`;
-    } else {
-      document.body.style.overflow = originalOverflow;
-      document.body.style.paddingRight = originalPaddingRight;
+    if (!isLocked) {
+      return;
     }
+
+    const originalOverflow = window.getComputedStyle(document.body).overflow;
+    const originalPaddingRight =
+      window.getComputedStyle(document.body).paddingRight;
+
+    const scrollbarWidth =
+      window.innerWidth - document.documentElement.clientWidth;
+
+    document.body.style.overflow = "hidden";
+    document.body.style.paddingRight = `${
+      parseFloat(originalPaddingRight || "0") + scrollbarWidth
+    }px`;
 
     return () => {
       document.body.style.overflow = originalOverflow;
