2e65f1d5301d313d10c2b7684ca01dd6ff6219a2
nanashima.m <nanashima.m@tech-studio.jp>
2026-07-30 22:45:08 +0900

    feat: ナビゲーションにContactを追加

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -5,5 +5,5 @@ export const NAV_ITEMS: NavItem[] = [
   { id: 2, name: "Skills", href: "#skills" },
   { id: 3, name: "Works", href: "#works" },
   { id: 4, name: "Portfolio", href: "#portfolio" },
-  //   { id: 5, name: "Contact", href: "#contact" }, // Contactセクション追加後に復活
+  { id: 5, name: "Contact", href: "#contact" },
 ];
